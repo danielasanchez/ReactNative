553b75982050bb20d5bcd86bc6bcdee19a08c717
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2020-11-06 22:54:19 -0800

    styles

---
+++ example2/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import Constants  from 'expo-constants';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={{flex:3}}>
+        <Text>Hola mundo desde {Platform.OS}!</Text>
+      </View>
+      <View style={{flex:2}}>
+        <Text>Hola!</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop:Constants.statusBarHeight
+  },
+  otroestilo:{
+    backgroundColor:'green'
+  }
+});
