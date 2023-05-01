6442ea847989286326c3cfa2868e176a1d7cf783
danielasanchez <danielasanchez.itt@hotmail.com>
2023-05-01 14:13:31 -0700

    Styles

--- example2/App.js
+++ example2/App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import Constants  from 'expo-constants';
-
+import { StyleSheet, Text, View, Plataform } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
